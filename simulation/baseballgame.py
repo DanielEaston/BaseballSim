839b43cbf3c571666ce99cfdce41a01b0e905b92
Daniel Easton <daniel.easton@thewarehouse.co.nz>
2021-06-22 13:34:04 +1200

    Created simulation module with baseball classes

---
+++ simulation/baseballgame.py
@@ -0,0 +1,17 @@
+class game():
+    def __init__(self, homeTeam, awayTeam):
+        self.homeScore = 0
+        self.awayScore = 0
+        self.awayTeam = awayTeam
+        self.homeTeam = homeTeam
+        self.inning = 1
+        self.inningBottom = False
+        self.first = []
+        self.second = []
+        self.third = []
+
+class player():
+    pass
+
+class team():
+    pass

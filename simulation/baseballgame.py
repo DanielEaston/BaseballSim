a84cd3cd8a8498ff1623a1c6f331dbe4627f59c5
Daniel Easton <daniel.easton@thewarehouse.co.nz>
2021-06-22 16:36:03 +1200

    Added baseball game play methods to game class

--- simulation/baseballgame.py
+++ simulation/baseballgame.py
@@ -4,16 +4,59 @@ class Game():
         self.awayScore = 0
         self.awayTeam = awayTeam
         self.homeTeam = homeTeam
+        self.awayPositionInOrder = 1
+        self.homePositionInOrder = 1
         self.inning = 1
         self.inningBottom = False
         self.first = []
         self.second = []
         self.third = []
-        self.gameOver = False
+        self.outs = 0
+        
     
     def play(self):
-        while not self.gameOver:
-            print("The Game Started")
+        while not self.isGameOver():
+            self.playInning()
+            self.isGameOver()
+            
+    
+    def playInning(self):
+        while self.outs < 3:
+            if self.inningBottom:
+                self.playAtBat(self.homeTeam.players[self.homePositionInOrder-1])
+            else:
+                self.playAtBat(self.awayTeam.players[self.awayPositionInOrder-1])
+
+        if self.inningBottom:
+            self.inningBottom = False
+            self.inning = self.inning + 1
+        else:
+            self.inningBottom = True
+    
+    def playAtBat(self, player):
+        import numpy.random as random
+        if random.random() <= 0.25:
+            self.updateBases(1, player)
+    
+    def getPlayersOnBase(self):
+        playersOnBase = []
+        playersOnBase.extend(self.third)
+        playersOnBase.extend(self.second)
+        playersOnBase.extend(self.first)
+
+    def updateBases(self, hit, player):
+        playersOnBase = self.getPlayersOnBase()
+        if self.inningBottom:
+            self.homeScore = self.homeScore + len(playersOnBase[0:hit])
+        else:
+            self.awayScore = self.awayScore + len(playersOnBase[0:hit])
+        
+
+
+
+
+    def isGameOver(self):
+        if self.inning >= 9 and self.inningBottom and self.homeScore != self.awayScore:
             self.gameOver = True
 
 class Player():

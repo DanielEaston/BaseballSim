6f97b3c6d47be595b3a0e4f3db5d307a2a6417fc
Daniel Easton <daniel.easton@thewarehouse.co.nz>
2021-06-23 16:47:09 +1200

    Functional game implemented
    -updateBases()
    -print statements added to track game progress
    -.5 chance of getting a double
    -removed player class
    -added player dict to Team class

--- simulation/baseballgame.py
+++ simulation/baseballgame.py
@@ -8,64 +8,100 @@ class Game():
         self.homePositionInOrder = 1
         self.inning = 1
         self.inningBottom = False
-        self.first = []
-        self.second = []
-        self.third = []
+        self.bases = [0,0,0]
         self.outs = 0
         
     
     def play(self):
+        
         while not self.isGameOver():
             self.playInning()
-            self.isGameOver()
+            
             
     
     def playInning(self):
+        print("Inning: "+str(self.inning))
+        print("Score: "+str(self.homeScore)+"-"+str(self.awayScore))
         while self.outs < 3:
             if self.inningBottom:
-                self.playAtBat(self.homeTeam.players[self.homePositionInOrder-1])
+                self.playAtBat(self.homeTeam.lineup[self.homePositionInOrder-1])
+                if self.homePositionInOrder == 9:
+                    self.homePositionInOrder = 1
+                else:
+                    self.homePositionInOrder = self.homePositionInOrder+1
             else:
-                self.playAtBat(self.awayTeam.players[self.awayPositionInOrder-1])
+                self.playAtBat(self.awayTeam.lineup[self.awayPositionInOrder-1])
+                if self.awayPositionInOrder == 9:
+                    self.awayPositionInOrder = 1
+                else:
+                    self.awayPositionInOrder = self.awayPositionInOrder+1
 
+        
         if self.inningBottom:
             self.inningBottom = False
             self.inning = self.inning + 1
         else:
             self.inningBottom = True
+        self.bases = [0,0,0]
+        self.outs = 0
     
     def playAtBat(self, player):
         import numpy.random as random
-        if random.random() <= 0.25:
-            self.updateBases(1, player)
+        print(player+" batting")
+        if random.random() <= 0.5:
+            self.updateBases(2, player)
+            print("Hit")
+            print(self.bases)
+            print("Score: "+str(self.homeScore)+"-"+str(self.awayScore))
+        else:
+            self.outs = self.outs+1
+            print("Out")
+            print(self.bases)
+            print("Score: "+str(self.homeScore)+"-"+str(self.awayScore))
     
-    def getPlayersOnBase(self):
-        playersOnBase = []
-        playersOnBase.extend(self.third)
-        playersOnBase.extend(self.second)
-        playersOnBase.extend(self.first)
+    
 
     def updateBases(self, hit, player):
-        playersOnBase = self.getPlayersOnBase()
+        
+        if hit == 4:
+            if self.inningBottom:
+                self.homeScore = self.homeScore + sum(self.bases) + 1
+            else:
+                self.awayScore = self.awayScore + sum(self.bases) + 1
+        
         if self.inningBottom:
-            self.homeScore = self.homeScore + len(playersOnBase[0:hit])
+            self.homeScore = self.homeScore + sum(self.bases[0-hit:])
         else:
-            self.awayScore = self.awayScore + len(playersOnBase[0:hit])
+            self.awayScore = self.awayScore + sum(self.bases[0-hit:])
+        
+        for i in range(-1,-4,-1):
+            if hit == 4:
+                self.bases = [0,0,0]
+            elif i - hit >= -3:
+                self.bases[i] = self.bases[i-hit]
+            else:
+                self.bases[hit-1] = 1
+                for j in range(hit-1):
+                    self.bases[j]=0
+                break
         
 
 
 
 
     def isGameOver(self):
-        if self.inning >= 9 and self.inningBottom and self.homeScore != self.awayScore:
-            self.gameOver = True
+        if self.inning >= 9 and self.inningBottom:
+            return True
+        else:
+            return False
 
-class Player():
-    def __init__(self, name):
-        self.name = name
-        self.battingAverage = 0
-        self.hits = 0
 
 class Team():
-    def __init__(self, name, players):
+    def __init__(self, name, playerNames):
         self.name = name
-        self.players = players
+        self.players = {}
+        self.lineup = playerNames
+        i=1
+        for name in playerNames:
+            self.players[name] = {"hits":0, "positionInOrder":i}
+            i=i+1
--- simulation/sim.py
+++ simulation/sim.py
@@ -1,6 +1,14 @@
 def run():
     from .baseballgame import Game
+    
+    from .baseballgame import Team
 
-    newGame = Game("Phillies", "Mets")
+    philliesLineup = ["Odubel", "Jean", "JT", "Bryce", "Rhys", "Andrew", "Ronald", "Alec", "Zack"]
+    metsLineup = ["Jeff", "Francisco", "Brad", "Dominic", "James", "Kevin", "Luis", "Albert", "Jacob"]
+
+    phillies = Team("Phillies", philliesLineup)
+    mets = Team("Mets", metsLineup)
+
+    newGame = Game(phillies, mets)
 
     newGame.play()
